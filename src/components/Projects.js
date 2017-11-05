008ac4aca16944186c7cb8b54b0966dad8514148
MikelSage <michael.sagapolutele@yahoo.com>
2017-11-05 14:40:44 -0700

    alter projects to show as accordion

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
-import { Header } from 'semantic-ui-react'
+import { Accordion, Icon } from 'semantic-ui-react'
 import axios from 'axios'
 import BASE_URL from '../requests'
 
 
 export default class Projects extends Component {
-  state = {}
+  state = {
+    activeIndex: -1
+  }
+
+  handleClick = (element, titleProps) => {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({
+      activeIndex: newIndex
+    })
+  }
+
   componentDidMount() {
     axios.get(BASE_URL + '/api/v1/current_projects')
       .then((response) => {
-        this.setState({projects: response})
+        this.setState({projects: response.data})
       })
       .catch((error) => {
         console.error(error);
@@ -17,25 +30,30 @@ export default class Projects extends Component {
   }
 
   render() {
-    const projects = this.state.projects && this.state.projects.data.map(function(project) {
+    const rawProjs = this.state.projects
+    const { activeIndex } = this.state
+    const projects = rawProjs && rawProjs.map((project, index) => {
       return (
-        <Header as='h3' textAlign='center' key={project.id}>
-          <Header.Content>
+        <div>
+          <Accordion.Title
+            active={activeIndex === index}
+            index={index}
+            onClick={this.handleClick}
+          >
+            <Icon name='dropdown'/>
             {project.name}
-          </Header.Content>
-        </Header>
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === index}>
+            <p>Some Stuff</p>
+          </Accordion.Content>
+        </div>
       )
     })
 
     return(
-      <div>
-        <Header as='h1' textAlign='center'>
-          <Header.Content>
-            This is where the projects will go
-          </Header.Content>
-        </Header>
+      <Accordion fluid styled>
         {projects}
-      </div>
+      </Accordion>
     )
   }
 }

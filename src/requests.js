614e140d8ab4e68b360cd0450f532cc897bdc176
MikelSage <michael.sagapolutele@yahoo.com>
2017-11-05 16:13:25 -0700

    change base url in requests

--- src/requests.js
+++ src/requests.js
@@ -1,5 +1,7 @@
 // import axios from 'axios'
-const BASE_URL = 'http://localhost:5000'
-// const BASE_URL = 'https://immense-thicket-45779.herokuapp.com'
+const enviro = process.env.NODE_ENV
+const localUrl = 'http://localhost:5000'
+const cloudUrl = 'https://immense-thicket-45779.herokuapp.com'
+const BASE_URL = enviro === 'production' ? cloudUrl : localUrl
 
 export default BASE_URL;

02e51eceb9cc01eb0f6ca75e51c8fa9b4830a11d
MikelSage <michael.sagapolutele@yahoo.com>
2017-11-05 21:44:47 -0700

    add ungraded submissions

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react'
+import { Accordion } from 'semantic-ui-react'
+import BASE_URL from '../requests'
+import axios from 'axios'
 
 export default class ProjectPage extends Component{
+  state = {}
+
+  componentDidMount() {
+    let project_id = this.props.match.params.id
+    axios.get(`${BASE_URL}/api/v1/projects/${project_id}/ungraded_subs`)
+      .then((response) => {
+        this.setState({submissions: response.data})
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }
+
+  mapSubmissions(rawSubs) {
+    return rawSubs.map((sub) => {
+      return {
+        title: {
+          content: `Submitted by: ${sub.user.first_name}, Github: ${sub.github_url}`,
+          key: `title-${sub.id}`
+        },
+        content: {
+          content: <div>Cool Stuff Goes Here</div>,
+          key: `content-${sub.id}`
+        }
+      }
+    })
+  }
+
   render() {
-    console.log(this.props);
-    return <div>Project {this.props.match.params.id} Page</div>
+    const rawSubs = this.state.submissions
+    const submissions = rawSubs && this.mapSubmissions(rawSubs)
+    return (
+      <Accordion fluid styled defaultActiveIndex={-1} panels={submissions}/>
+    )
   }
 }

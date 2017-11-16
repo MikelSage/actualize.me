d26eab9ec03ae796c133654d902b9218b0eb53db
MikelSage <michael.sagapolutele@yahoo.com>
2017-11-16 10:18:08 -0700

    add conditional link to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 export default class Home extends Component {
+  authorizedInstructor() {
+    return localStorage.getItem('user_id')
+      && localStorage.getItem('role') === 'instructor'
+  }
+
+  authorizedStudent() {
+    return localStorage.getItem('user_id')
+      && localStorage.getItem('role') === 'student'
+  }
+
   render() {
+    const link = this.authorizedInstructor() ?
+        <Link to='/projects/new'>Add New Project</Link> :
+        <Link to='/submissions/new'>Submit a Project</Link>
     return (
       <div>
         <h1 className='ui center aligned header'>
           {`Welcome, ${localStorage.getItem('username')}`}
         </h1>
-        <Link to='/projects/new'>Add New Project</Link>
+        {link}
       </div>
     )
   }
